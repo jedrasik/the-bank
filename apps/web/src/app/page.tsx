1ea8083b113985960452b96a27877c7f55969d20
jedrasik <adam.jedrasik@nordcloud.com>
2023-09-28 11:42:58 +0200

    some styling

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-1 flex-col items-center justify-center bg-gray-800">
-      <h1 className="text-7xl text-gray-300">👋 Hello </h1>
+    <main className="flex h-screen flex-1 flex-col items-center justify-center bg-gradient-to-b from-slate-50 to-slate-600">
+      <div className="gap-8 columns-2 text-white">
+        <div className="h-14 justify-center text-5xl">The Bank</div>
+        <div className="h-14 justify-center text-5xl">My profile</div>
+      </div>
     </main>
   );
 }
